f1669fcf5b22f6b815d6938fc2e217ece0824d38
medobarakat <ahmedbarakat2401@gmail.com>
2022-09-14 23:05:17 +0200

    ending login screen

--- src/Screens/Auth/Login.js
+++ src/Screens/Auth/Login.js
@@ -100,6 +100,13 @@ const styles = StyleSheet.create({
   },
   btn: {
     marginTop: height / 13,
+    borderRadius: height / 5,
+    elevation: 3,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    shadowColor: "#52006A",
   },
   lastsec: {
     display: "flex",

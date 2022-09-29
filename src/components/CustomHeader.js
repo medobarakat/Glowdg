d3fdefc695fb9cd03b31bcc69be6a7ac002a2600
medobarakat <ahmedbarakat2401@gmail.com>
2022-09-29 10:19:21 +0200

    adding feature

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -1,13 +1,16 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Header, Icon } from "@rneui/themed";
 import { PrimaryColor, BlackColor, WhiteColor } from "../constants/Colors";
 import { useNavigation } from "@react-navigation/native";
 import { TitleSize } from "../constants/Sized";
+import { Menu, MenuItem, MenuDivider } from "react-native-material-menu";
 
 const CustomHeader = () => {
+  const [visible, setVisible] = useState(false);
   const navigation = useNavigation();
-
+  const hideMenu = () => setVisible(false);
+  const showMenu = () => setVisible(true);
   return (
     <View>
       <Header
@@ -26,9 +29,17 @@ const CustomHeader = () => {
         }
         rightComponent={
           <View>
-            <TouchableOpacity>
-              <Icon name="person" color="white" />
-            </TouchableOpacity>
+            <Menu
+              visible={visible}
+              anchor={<Icon onPress={showMenu} name="person" color="white" />}
+              onRequestClose={hideMenu}
+            >
+              <MenuItem onPress={hideMenu}>Menu item 1</MenuItem>
+              <MenuItem onPress={hideMenu}>Menu item 2</MenuItem>
+              <MenuItem disabled>Disabled item</MenuItem>
+              <MenuDivider />
+              <MenuItem onPress={hideMenu}>Menu item 4</MenuItem>
+            </Menu>
           </View>
         }
       />

19ce098263bd48267e65194110f989047cffabf3
medobarakat <ahmedbarakat2401@gmail.com>
2022-11-01 10:30:47 +0200

    working on the transulation files

--- src/Screens/Bussiness.js
+++ src/Screens/Bussiness.js
@@ -1,14 +1,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { PrimaryColor, BlackColor, WhiteColor } from "../constants/Colors";
-import {
-  height,
-  width,
-  smallSize,
-  verysmallSize,
-  secSmallSize,
-  TitleSize,
-} from "../constants/Sized";
+import { BlackColor } from "../constants/Colors";
+import { height, TitleSize } from "../constants/Sized";
 import { useTranslation } from "react-i18next";
 
 const Bussiness = () => {

1baa0448483d86514826dafc4e9e1675c1d63feb
medobarakat <ahmedbarakat2401@gmail.com>
2022-11-08 10:16:44 +0200

    adding whatsapp icon

--- src/Navigation/Paths.js
+++ src/Navigation/Paths.js
@@ -1,20 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // the screens
 import HomeNavigation from "../Navigation/HomeNavigation";
 // auth screen
-import Login from "../Screens/Auth/Login";
-import Signup from "../Screens/Auth/Signup";
-import { LogBox } from 'react-native';
-
-// import { useSelector } from "react-redux";
+import { LogBox } from "react-native";
 
 const Stack = createNativeStackNavigator();
-// if user logged
 const Renderpages = () => {
-  // const IsUser = useSelector((state) => state.auth.IsUser);
   return (
     <>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -23,40 +17,13 @@ const Renderpages = () => {
     </>
   );
 };
-// // if no user logged (guest)
-
-// const Renderpages2 = () => {
-//   const IsGuest = useSelector((state) => state.auth.IsGuest);
-
-//   return (
-//     <>
-//       {IsGuest ? (
-//         <Stack.Navigator screenOptions={{ headerShown: false }}>
-//           <Stack.Screen name="home" component={Home} />
-//         </Stack.Navigator>
-//       ) : (
-//         //! Here if logged
-//         <Stack.Navigator
-//           initialRouteName="Login"
-//           screenOptions={{ headerShown: false }}
-//         >
-//           <Stack.Screen name="Login" component={Login} />
-//           <Stack.Screen name="signup" component={Signup} />
-//           <Stack.Screen name="home1" component={HomeNavigation} />
-//         </Stack.Navigator>
-//       )}
-//     </>
-//   );
-// };
 
 function Paths() {
-  // const IsLogged = useSelector((state) => state.auth.IsLogged);
   useEffect(() => {
-    LogBox.ignoreAllLogs()
-  })
+    LogBox.ignoreAllLogs();
+  });
   return (
     <NavigationContainer>
-      {/* {IsLogged ? <Renderpages /> : <Renderpages2 />} */}
       <Renderpages />
     </NavigationContainer>
   );

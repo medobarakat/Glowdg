0773364233528e7c5e91ce95f0f328ae0cf7337f
medobarakat <ahmedbarakat2401@gmail.com>
2022-12-11 14:07:03 +0200

    adding aboutus page

--- src/Screens/Whoarewe.js
+++ src/Screens/Whoarewe.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import React from "react";
 import { PrimaryColor, BlackColor, WhiteColor } from "../constants/Colors";
 import {
@@ -14,9 +14,19 @@ import { useTranslation } from "react-i18next";
 const Whoarewe = () => {
   const { t } = useTranslation();
   return (
-    <View style={styles.container}>
-      <Text style={styles.maintxt}>{t("WhoWeAre")}</Text>
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <Text style={styles.maintxt}>{t("AboutUs")}</Text>
+        <Image source={require("../img/whoweare.jpeg")} style={styles.img} />
+        <View style={styles.txtContainer}>
+          <Text style={styles.txt1}>OUR STORY</Text>
+          <Text style={styles.txt2}>
+            Providing easy , fast and innovative retail and services experience
+            to the UAE community
+          </Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -34,5 +44,27 @@ const styles = StyleSheet.create({
     fontSize: TitleSize / 2,
     fontFamily: "Roboto_700Bold",
     color: BlackColor,
+  },
+  img: {
+    width: width / 1.2,
+    height: height / 2,
+    marginTop: height / 15,
+  },
+  txtContainer: {
+    marginHorizontal: width / 10,
+    marginTop: height / 55,
+  },
+  txt1: {
+    marginBottom: height / 55,
+    fontSize: secSmallSize / 1.2,
+    fontFamily: "Roboto_500Medium",
+    color: "#777",
+  },
+  txt2: {
+    marginLeft: width / 20,
+    marginHorizontal: height / 50,
+    fontFamily: "Roboto_500Medium",
+    fontSize: secSmallSize,
+    lineHeight: height / 25,
   },
 });
